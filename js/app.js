36d6f63dd547fa9be3b039d1ab7b31fcba33205e
Rohit Joshi <rohitjoshi0095@gmail.com>
2023-02-06 11:35:14 +0530

    Transfer Project

---
+++ js/app.js
@@ -0,0 +1,34 @@
+let string = "";
+
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button)=>{
+
+      button.addEventListener("click", (e)=>{
+    //    console.log(e.target)
+
+        
+    if(e.target.innerHTML === "="){
+        string = eval(string);
+   
+      //  console.log(string)
+
+        document.querySelector(".value").value = string
+    }
+
+   else if(e.target.innerHTML === "C"){
+        string = "";
+   
+      //  console.log(string)
+
+        document.querySelector(".value").value = string
+    }
+
+
+    else{
+        string = string + e.target.innerHTML;
+    //    console.log(string)
+
+        document.querySelector(".value").value = string
+    }
+      })
+})
